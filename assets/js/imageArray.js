4efc87b3468195fd67adfceb80a52256a13a7df3
Lucian <luciannovo@gmail.com>
2014-05-25 22:32:59 -0700

    Completed side bar scrolling and images scrolling. So happay. Next step is to add padding before filling in copy

--- assets/js/imageArray.js
+++ assets/js/imageArray.js
@@ -1,67 +1,105 @@
-var images = [
-"assets/imgs/BID.jpg",
+  var images = [
+  "assets/imgs/BID.jpg",
 
-"assets/imgs/collagePuzzle/collage_example.png",
+  "assets/imgs/collagePuzzle/collage_example.png",
 
-"assets/imgs/collagePuzzle/collagePlay.jpg",
+  "assets/imgs/collagePuzzle/collagePlay.jpg",
 
-"assets/imgs/paperplanes/seats.jpg",
+  "assets/imgs/paperplanes/seats.jpg",
 
-"assets/imgs/paperplanes/paperAirplanePair_recommended.png",
+  "assets/imgs/paperplanes/paperAirplanePair_recommended.png",
 
-"assets/imgs/paperplanes/airplaneHeave.jpg",
+  "assets/imgs/paperplanes/airplaneHeave.jpg",
 
-"assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_0.png",
+  "assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_0.png",
 
-"assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_1.png",
+  "assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_1.png",
 
-"assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_2.png",
+  "assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_2.png",
 
-"assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_3.png",
+  "assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_3.png",
 
-"assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_4.png",
+  "assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_4.png",
 
-"assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_5.png",
+  "assets/imgs/Jr_project/InsideOut_TEDx2014_LucianNovosel_5.png",
 
-"assets/imgs/Jr_project/quadShoot.jpg",
+  "assets/imgs/Jr_project/quadShoot.jpg",
 
-"assets/imgs/Jr_project/stageTalk.jpg",
+  "assets/imgs/Jr_project/stageTalk.jpg",
 
-"assets/imgs/peoplePuzzles/childGroup.jpg",
+  "assets/imgs/peoplePuzzles/childGroup.jpg",
 
-"assets/imgs/peoplePuzzles/familyGroup.jpg",
+  "assets/imgs/peoplePuzzles/familyGroup.jpg",
 
-"assets/imgs/peoplePuzzles/group.jpg",
+  "assets/imgs/peoplePuzzles/group.jpg",
 
-"assets/imgs/peoplePuzzles/peoplePuzzles_fb3.jpg",
+  "assets/imgs/peoplePuzzles/peoplePuzzles_fb3.jpg",
 
-"assets/imgs/peoplePuzzles/peoplePuzzles_fb4.jpg",
+  "assets/imgs/peoplePuzzles/peoplePuzzles_fb4.jpg",
 
-"assets/imgs/peoplePuzzles/peoplePuzzles_fb5.jpg",
+  "assets/imgs/peoplePuzzles/peoplePuzzles_fb5.jpg",
 
-"assets/imgs/stageDesign/chalkboards.jpg",
+  "assets/imgs/stageDesign/chalkboards.jpg",
 
-"assets/imgs/stageDesign/Mauren_Prototype.jpg",
+  "assets/imgs/stageDesign/Mauren_Prototype.jpg",
 
-"assets/imgs/stageDesign/protowing.jpg",
+  "assets/imgs/stageDesign/protowing.jpg",
 
-"assets/imgs/stageDesign/stage.jpg"];
+  "assets/imgs/stageDesign/stage.jpg"];
 
-for(var i=0; i<images.length; i++){
-	$('#pictures').append("<img " + "id=\"" + i + "\" class=\"ui huge image slide\" src=\"" + images[i] + "\" style=\" width: 100%; height:auto;\">");
-}
+  for(var i=0; i<images.length; i++){
+  	$('#pictures').append(" <a href=\"#" +  i + "\"> <img " + "id=\"" + i + "\" class=\"ui huge image slide\" src=\"" + images[i] + "\" style=\" width: 100%; height:auto;\"></\\a>");
+  }
 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+  var $nonScrollable = $("#text");
+
+  function disableManager(length){
+      $nonScrollable.disablescroll();
+      setTimeout($nonScrollable.enablescroll,length);
+  }
+
+  $("#text").bind('mousewheel', function(event) {
+      if (event.originalEvent.wheelDelta >= 0) {
+          scrollArticleUp(1500);
+          console.log('Scroll up');
       }
-    }
+      else {
+          scrollArticleDown(1500);
+          console.log('Scroll down');
+      }
   });
-});
+
+  function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;  
+  }
+
+  function scrollArticleUp(length){
+    if($('.article.active').next().length > 0){
+       //temporarily disable the scroll
+       disableManager(length);
+       //update current active article
+       $(".article.active").next().addClass("active");
+       $(".active").eq(0).removeClass("active");
+       //get current article
+       //scroll to next article
+       $("#text").scrollTo($(".active"),length);
+       $("#pictures").scrollTo($("#1"),length);
+    }
+  }
+
+  function scrollArticleDown(length){
+      if($('.article.active').prev().length > 0){
+         //temporarily disable the scroll
+         disableManager(length); 
+         // upscroll code
+         $(".article.active").prev().addClass("active");
+         $(".active").eq(1).removeClass("active");
+         //get current article
+         //scroll to next article
+         $("#text").scrollTo($(".active"),length);
+         $("#pictures").scrollTo($("#14"),length);
+      }
+  }
